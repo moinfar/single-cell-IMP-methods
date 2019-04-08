2260f73d61e0095e9927878974992817ad79f5d2
Amir Ali Moinfar <amir@debian8svm.debian8svm>
2019-04-08 07:33:18 -0400

    BugFix

--- containers/scVI/installer-gpu.sh
+++ containers/scVI/installer-gpu.sh
@@ -1,2 +1,6 @@
+apt-get update -y
+apt-get install build-essential python3-dev libxml2 libxml2-dev zlib1g-dev -y
+pip install --no-cache-dir python-igraph
+
 # install scvi
 pip install --no-cache-dir scvi
