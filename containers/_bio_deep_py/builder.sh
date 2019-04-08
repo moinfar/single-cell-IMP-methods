8a1a3dc4445336984c987f7bd9a63f8dbc777001
moinfar <moinfar.amirali@gmail.com>
2019-04-08 16:55:30 +0430

    Minor change in name

--- containers/_bio_deep_py/builder.sh
+++ containers/_bio_deep_py/builder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile-tensorflow -t moinfar/bio-deep-python:tensorflow-3.6 .
-docker build -f Dockerfile-pytorch -t moinfar/bio-deep-python:pytorch-3.6 .
+docker build -f Dockerfile-tensorflow -t moinfar/bio-deep-python:tensorflow-py36 .
+docker build -f Dockerfile-pytorch -t moinfar/bio-deep-python:pytorch-py36 .
