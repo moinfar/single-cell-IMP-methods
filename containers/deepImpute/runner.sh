b94f600152525f6a55d1f0d55702ab9cc74a5965
Amir Ali Moinfar <amir@debian8svm.debian8svm>
2019-05-11 09:45:29 -0400

    BugFix

--- containers/deepImpute/runner.sh
+++ containers/deepImpute/runner.sh
@@ -6,7 +6,7 @@ export outdir=$2
 
 mkdir $outdir
 
-bash /time_it.sh /tmp/time.txt python /app/deepimpute/deepImpute.py --cell-axis columns -o "$outdir/deepimpute.csv" "$inputpath"
+bash /time_it.sh /tmp/time.txt python /app/deepimpute/deepImpute.py --cell-axis columns -o "$outdir/deepimpute.csv" "$inputpath" "${@:3}"
 cp /tmp/time.txt "$outdir/time.txt"
 
 python transpose_it.py -i "$outdir/deepimpute.csv" -o "$outdir/deepimpute.csv"
